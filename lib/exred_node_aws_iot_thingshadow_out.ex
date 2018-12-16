e4c6d9ede6bb2f15c788356c9aab2cc66eda8ce7
Zsolt <zsolt@localhost.localdomain>
2018-12-15 22:06:29 -0500

    use new nodeprototype

--- lib/exred_node_aws_iot_thingshadow_out.ex
+++ lib/exred_node_aws_iot_thingshadow_out.ex
@@ -1,11 +1,11 @@
 defmodule Exred.Node.AwsIotThingShadowOut do
   @moduledoc """
   Publishes messages to AWS IoT Cloud.
-  
+
   The incoming message needs to have a valid AWS topic in the topic field.
-  
+
   Incoming msg format:
-  
+
   ```elixir
   msg = %{
     topic   :: [binary] | binary
@@ -22,17 +22,23 @@ defmodule Exred.Node.AwsIotThingShadowOut do
     thing_name: %{type: "string", value: "myThing"},
     name: %{value: @name, type: "string", attrs: %{max: 20}}
   }
-  @ui_attributes %{fire_button: false, right_icon: "send" }
-  
-  use Exred.Library.NodePrototype
+  @ui_attributes %{fire_button: false, right_icon: "send"}
+
+  use Exred.NodePrototype
   require Logger
 
-  
   @impl true
   def handle_msg(msg, state) do
-    encoded_payload = Poison.encode! msg.payload
-    res = AwsIotClient.publish msg.topic, encoded_payload, Map.get(msg, :qos, 0), Map.get(msg, :retain, false)
-    { %{msg | payload: res}, state }
-  end
+    encoded_payload = Poison.encode!(msg.payload)
 
+    res =
+      AwsIotClient.publish(
+        msg.topic,
+        encoded_payload,
+        Map.get(msg, :qos, 0),
+        Map.get(msg, :retain, false)
+      )
+
+    {%{msg | payload: res}, state}
+  end
 end
